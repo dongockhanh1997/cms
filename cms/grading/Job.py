cbd1fd5a9d6d05e6d87537d12d8e263a1fdce507
Khanh Do Ngoc <dongockhanh1997@gmail.com>
2014-06-09 17:01:17 +0700

    Update copyright.

--- cms/grading/Job.py
+++ cms/grading/Job.py
@@ -6,6 +6,7 @@
 # Copyright © 2013 Luca Wehrstedt <luca.wehrstedt@gmail.com>
 # Copyright © 2013 Bernard Blackham <bernard@largestprime.net>
 # Copyright © 2013 Stefano Maggiolo <s.maggiolo@gmail.com>
+# Copyright © 2014 Khanh Do Ngoc <dongockhanh1997@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
--- cms/grading/ScoreType.py
+++ cms/grading/ScoreType.py
@@ -6,6 +6,7 @@
 # Copyright © 2010-2013 Stefano Maggiolo <s.maggiolo@gmail.com>
 # Copyright © 2010-2012 Matteo Boscariol <boscarim@hotmail.com>
 # Copyright © 2013 Luca Wehrstedt <luca.wehrstedt@gmail.com>
+# Copyright © 2014 Khanh Do Ngoc <dongockhanh1997@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
